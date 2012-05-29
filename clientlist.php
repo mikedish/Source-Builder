2bc65415b0488a1ce12e5b23acbedbe1b891e4b5
Asher Cohen <asherlcohen@gmail.com>
2012-05-29 14:50:07 -0700

    navbar to client list

---
+++ clientlist.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Trilogy Source Code Builder</title>
+<link rel="ICON" href="favicon.ico" />
+<link rel="SHORTCUT ICON" href="favicon.ico" />
+<link rel="Stylesheet" href="css/jquery-ui-1.8.16.custom.css" type="text/css" />  
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/chosen.css" />
+<style type="text/css">
+.main {margin-top: 60px;}
+.ui-datepicker-today a {border: 1px solid #333 !important;}
+#generated-text {width: 300px;}
+label.error {color: #F00; width: 100%; text-align: left; display: none;}
+input.error {border-color: #F00;}
+#ATreminder, #ADreminder {color: #F00; display: none;}
+</style>
+
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+<iframe src="clients/index.php" width="100%" height="1000" frameborder="0" class="main" />
+</body></html>
+
+
