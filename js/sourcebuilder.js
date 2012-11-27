c3c778d1f41c176f8bf304f85e30d08f65a4386b
Asher Cohen <asherlcohen@gmail.com>
2012-11-27 14:13:48 -0800

    make optional params collapse on deselct

--- js/sourcebuilder.js
+++ js/sourcebuilder.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    var $form = $('form.validate')
     $('[name="date"]').datepicker({ format: 'yyyy-mm-dd'})
 
     function output(code) {
@@ -14,41 +13,43 @@ $(document).ready(function() {
       }
     }
 
-    $('[value="ad"]').on('change', function(e) {
-        $('.collapse.ad').collapse()
+    $('[value="ad"], [value="add"]').on('change', function(e) {
+        var selector = '.collapse.' + this.value
+        $(selector).collapse('show')
     })
-    $('[value="add"]').on('change', function(e) {
-      $('.collapse.addthis').collapse()
+    $('[name="medium"]').on('click', function(e) {
+        $('.collapse.in').collapse('hide')
     })
     $('[value="imp"]').on('change', function(e) {
-      $('#base').removeClass('required')
+        $('#base').removeClass('required')
     })
+   
 
     $('#generate-url').on('click', function(e) {
         e.preventDefault()
         inputs = {
-          base: document.getElementById('base').value,
-          client: $('#client').val(),
-          platform: platform(),
-          medium: $('input[name=medium]:checked').val(), 
-          date: document.getElementById('date').value,
-          campaign: document.getElementById('campaign').value,
-          additional: document.getElementById('additional').value,
-          adProperties: {
-            network: document.getElementById('adProperties[network]').value,
-            geoTargeting: document.getElementById('adProperties[geoTargeting]').value,
-            flight: document.getElementById('adProperties[flight]').value,
-            variation: document.getElementById('adProperties[variation]').value
-          },
-          addthisProperties: {
-            title: document.getElementById('addthisProperties[title]').value,
-            description: document.getElementById('addthisProperties[description]').value
-          }
-        }
+            base: document.getElementById('base').value,
+            client: $('#client').val(),
+            platform: platform(),
+            medium: $('input[name=medium]:checked').val(), 
+            date: document.getElementById('date').value,
+            campaign: document.getElementById('campaign').value,
+            additional: document.getElementById('additional').value,
+            adProperties: {
+                network: document.getElementById('adProperties[network]').value,
+                geoTargeting: document.getElementById('adProperties[geoTargeting]').value,
+                flight: document.getElementById('adProperties[flight]').value,
+                variation: document.getElementById('adProperties[variation]').value
+            },
+            addthisProperties: {
+                title: document.getElementById('addthisProperties[title]').value,
+                description: document.getElementById('addthisProperties[description]').value
+            }
+        }  
 
         generatedUrl = new Url(inputs)
         
-        if ($form.valid()) {
+        if ($('form.validate').valid()) {
 //        if (true) {
             if (inputs.medium === 'add') {
                 var generatedAddthisCode = new AddthisCode(generatedUrl)
